e01f93be295a78131b1581ed516efd994d782d58
huangboju <529940945@qq.com>
2017-03-22 21:47:48 +0800

    coding QMUICommonDefines

--- QMUI.swift/QMUIKit/UICommon/QMUICommonDefines.swift
+++ QMUI.swift/QMUIKit/UICommon/QMUICommonDefines.swift
@@ -120,6 +120,129 @@ let UIFontBoldWithFont: (UIFont) -> UIFont = { UIFont.boldSystemFont(ofSize: $0.
 let UIFontLightMake: (CGFloat) -> UIFont = { UIFont.qmui_lightSystemFont(ofSize: $0) }
 let UIFontLightWithFont: (UIFont) -> UIFont = { UIFont.qmui_lightSystemFont(ofSize: $0.pointSize) }
 let UIDynamicFontMake: (CGFloat) -> UIFont = { UIFont.qmui_dynamicFont(with: $0, bold: false) }
+let UIDynamicFontMakeWithLimit: (CGFloat, CGFloat, CGFloat) -> UIFont = { UIFont.qmui_dynamicFont(with: $0.0, upperLimitSize: $0.1, lowerLimitSize: $0.2, bold: false) }
+let UIDynamicFontBoldMake: (CGFloat) -> UIFont = { UIFont.qmui_dynamicFont(with: $0, bold: true) }
+let UIDynamicFontBoldMakeWithLimit: (CGFloat, CGFloat, CGFloat) -> UIFont = { UIFont.qmui_dynamicFont(with: $0.0, upperLimitSize: $0.1, lowerLimitSize: $0.2, bold: true) }
+
+
+// MARK: - 数学计算
+
+let AngleWithDegrees: (CGFloat) -> CGFloat = { .pi * $0 / 180.0 }
+
+
+// MARK: - 动画
+// TODO:
+//let QMUIViewAnimationOptionsCurveOut (7<<16)
+//let QMUIViewAnimationOptionsCurveIn (8<<16)
+
+
+// MARK: - 其他
+// TODO:
+//#define QMUILog(...) [[QMUIHelper sharedInstance] printLogWithCalledFunction:__FUNCTION__ log:__VA_ARGS__]
+
+
+
+/**
+ *  基于指定的倍数，对传进来的 floatValue 进行像素取整。若指定倍数为0，则表示以当前设备的屏幕倍数为准。
+ *
+ *  例如传进来 “2.1”，在 2x 倍数下会返回 2.5（0.5pt 对应 1px），在 3x 倍数下会返回 2.333（0.333pt 对应 1px）。
+ */
+
+func flatSpecificScale(_ value: CGFloat, _ scale: CGFloat) -> CGFloat {
+    let s = scale == 0 ? ScreenScale : scale
+    return ceil(value * s) / s
+}
+
+/**
+ *  基于当前设备的屏幕倍数，对传进来的 floatValue 进行像素取整。
+ *
+ *  注意如果在 Core Graphic 绘图里使用时，要注意当前画布的倍数是否和设备屏幕倍数一致，若不一致，不可使用 flat() 函数，而应该用 flatSpecificScale
+ */
+
+func flat(_ value: CGFloat) -> CGFloat {
+    return flatSpecificScale(value, 0)
+}
+
+/**
+ *  类似flat()，只不过 flat 是向上取整，而 floorInPixel 是向下取整
+ */
+func floorInPixel(_ value: CGFloat) -> CGFloat {
+    let resultValue = floor(value * ScreenScale) / ScreenScale
+    return resultValue
+}
+
+func between(_ minimumValue: CGFloat, _ value: CGFloat, _ maximumValue: CGFloat) -> Bool {
+    return minimumValue < value && value < maximumValue
+}
+
+func betweenOrEqual(_ minimumValue: CGFloat, _ value: CGFloat, _ maximumValue: CGFloat) -> Bool {
+    return minimumValue <= value && value <= maximumValue
+}
+
+func ReplaceMethod(_ _class: AnyClass, _originSelector: Selector, _newSelector: Selector) {
+    let oriMethod = class_getInstanceMethod(_class, _originSelector)
+    let newMethod = class_getInstanceMethod(_class, _newSelector)
+    let isAddedMethod = class_addMethod(_class, _originSelector, method_getImplementation(newMethod), method_getTypeEncoding(newMethod))
+    if (isAddedMethod) {
+        class_replaceMethod(_class, _newSelector, method_getImplementation(oriMethod), method_getTypeEncoding(oriMethod))
+    } else {
+        method_exchangeImplementations(oriMethod, newMethod)
+    }
+}
+
+
+// MARK: - CGFloat
+
+/// 用于居中运算
+
+func CGFloatGetCenter(_ parent: CGFloat, _ child: CGFloat) -> CGFloat {
+    return flat((parent - child) / 2.0)
+}
+
+
+// MARK: - CGPoint
+
+/// 两个point相加
+
+extension CGPoint {
+    func union(_ point: CGPoint) -> CGPoint {
+        return CGPoint(x: flat(x + point.x), y: flat(y + point.y))
+    }
+}
+
+extension CGRect {
+    /// 获取rect的center，包括rect本身的x/y偏移
+    var center: CGPoint {
+        return CGPoint(x: flat(midX), y: flat(midY))
+    }
+}
+
+extension CGSize {
+    var center: CGPoint {
+        return CGPoint(x: flat(width / 2.0), y: flat(height / 2.0))
+    }
+
+    /// 判断一个size是否为空（宽或高为0）
+    var isEmpty: Bool {
+        return width <= 0 || height <= 0
+    }
+
+    /// 将一个CGSize像素对齐
+    var flatted: CGSize {
+        return CGSize(width: flat(width), height: flat(height))
+    }
+
+    /// 将一个 CGSize 以 pt 为单位向上取整
+    var sizeCeil: CGSize {
+        return CGSize(width: ceil(width), height: ceil(height))
+    }
+
+    /// 将一个 CGSize 以 pt 为单位向下取整
+    var sizeFloor: CGSize {
+        return CGSize(width: floor(width), height: floor(height))
+    }
+}
+
 
 
 // MARK: - UIEdgeInsets
@@ -127,20 +250,36 @@ let UIDynamicFontMake: (CGFloat) -> UIFont = { UIFont.qmui_dynamicFont(with: $0,
 extension UIEdgeInsets {
     /// 获取UIEdgeInsets在水平方向上的值
     var horizontalValue: CGFloat {
-        return self.left + self.right
+        return left + right
     }
 
     /// 获取UIEdgeInsets在垂直方向上的值
     var verticalValue: CGFloat {
-        return self.top + self.bottom
+        return top + bottom
     }
 
     /// 将两个UIEdgeInsets合并为一个
-    func concat(_ insets1: UIEdgeInsets, _ insets2: UIEdgeInsets) -> UIEdgeInsets {
-        let top = insets1.top + insets2.top
-        let left = insets1.left + insets2.left
-        let bottom = insets1.bottom + insets2.bottom
-        let right = insets1.right + insets2.right
+    func concat(insets: UIEdgeInsets) -> UIEdgeInsets {
+        let top = self.top + insets.top
+        let left = self.left + insets.left
+        let bottom = self.bottom + insets.bottom
+        let right = self.right + insets.right
         return UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+    }
+
+    mutating func setTop(_ top: CGFloat) {
+        self.top = flat(top)
+    }
+
+    mutating func setLeft(_ left: CGFloat) {
+        self.left = flat(left)
+    }
+
+    mutating func setBottom(bottom: CGFloat) {
+        self.bottom = flat(bottom)
+    }
+
+    mutating func setRight(_ right: CGFloat) {
+        self.right = flat(right)
     }
 }
