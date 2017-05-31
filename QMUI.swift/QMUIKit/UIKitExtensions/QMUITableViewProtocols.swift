a14d1dcf368d166832a43f11e77a216743b1e95f
huangboju <529940945@qq.com>
2017-05-31 17:08:53 +0800

    UIScrollView+QMUI is done

--- QMUI.swift/QMUIKit/UIKitExtensions/QMUITableViewProtocols.swift
+++ QMUI.swift/QMUIKit/UIKitExtensions/QMUITableViewProtocols.swift
@@ -19,11 +19,6 @@ extension qmui_UITableViewDataSource {
 protocol QMUITableViewDelegate: UITableViewDelegate {
 
     /**
-     * 控制是否在列表顶部显示搜索框。在QMUICommonTableViewController里已经接管了searchBar的初始化工作，所以外部只需要控制“显示/隐藏”，不需要自己再初始化一次。
-     */
-    func shouldShowSearchBar(in tableView: QMUITableView) -> Bool
-
-    /**
      * 自定义要在<i>- (BOOL)touchesShouldCancelInContentView:(UIView *)view</i>内的逻辑<br/>
      * 若delegate不实现这个方法，则默认对所有UIControl返回NO（UIButton除外，它会返回YES），非UIControl返回YES。
      */
@@ -32,7 +27,7 @@ protocol QMUITableViewDelegate: UITableViewDelegate {
 
 protocol QMUITableViewDataSource: UITableViewDataSource, qmui_UITableViewDataSource {}
 
-// 为了解决可选
+
 extension QMUITableViewDelegate {
     func shouldShowSearchBar(in _: QMUITableView) -> Bool {
         return false
--- QMUI.swift/QMUIKit/UIKitExtensions/UIScrollView+QMUI.swift
+++ QMUI.swift/QMUIKit/UIKitExtensions/UIScrollView+QMUI.swift
@@ -7,12 +7,54 @@
 //
 
 extension UIScrollView {
+    
+    private struct AssociatedKeys {
+        static var description = "description"
+    }
+
+    private static let _onceToken = UUID().uuidString
+    
+    open override class func initialize() {
+        DispatchQueue.once(token: _onceToken) {
+            ReplaceMethod(self, #selector(description), #selector(qmui_description))
+        }
+    }
+
+    func qmui_description() -> String {
+        return self.qmui_description() + ", contentInset = \(contentInset)"
+    }
+
+    /// 判断UIScrollView是否已经处于顶部（当UIScrollView内容不够多不可滚动时，也认为是在顶部）
+    public var qmui_alreadyAtTop: Bool {
+        if qmui_canScroll {
+            return true
+        }
+
+        if contentOffset.y == -contentInset.top {
+            return true
+        }
+
+        return false
+    }
+
+    /// 判断UIScrollView是否已经处于底部（当UIScrollView内容不够多不可滚动时，也认为是在底部）
+    public var qmui_alreadyAtBottom: Bool {
+        if !qmui_canScroll {
+            return true
+        }
+
+        if contentOffset.y == contentSize.height + contentInset.bottom - bounds.height {
+            return true
+        }
+
+        return false
+    }
 
     /**
      * 判断当前的scrollView内容是否足够滚动
      * @warning 避免与<i>scrollEnabled</i>混淆
      */
-    var qmui_canScroll: Bool {
+    public var qmui_canScroll: Bool {
         // 没有高度就不用算了，肯定不可滚动，这里只是做个保护
         if bounds.size == .zero {
             return false
@@ -28,7 +70,7 @@ extension UIScrollView {
      * @param force 是否无视qmui_canScroll而强制滚动
      * @param animated 是否用动画表现
      */
-    func qmui_scrollToTopForce(_ force: Bool, animated: Bool) {
+    public func qmui_scrollToTopForce(_ force: Bool, animated: Bool) {
         if force || (!force && qmui_canScroll) {
             setContentOffset(CGPoint(x: -contentInset.left, y: -contentInset.top), animated: animated)
         }
@@ -37,12 +79,12 @@ extension UIScrollView {
     /**
      * 等同于qmui_scrollToTop(false, animated: animated)
      */
-    func qmui_scrollToTopAnimated(_ animated: Bool) {
+    public func qmui_scrollToTopAnimated(_ animated: Bool) {
         qmui_scrollToTopForce(false, animated: animated)
     }
 
     /// 等同于qmui_scrollToTop(false)
-    func qmui_scrollToTop() {
+    public func qmui_scrollToTop() {
         qmui_scrollToTopAnimated(false)
     }
 
@@ -51,19 +93,19 @@ extension UIScrollView {
      * @param animated 是否用动画表现
      * @see [UIScrollView qmui_canScroll]
      */
-    func qmui_scrollToBottomAnimated(_ animated: Bool) {
+    public func qmui_scrollToBottomAnimated(_ animated: Bool) {
         if qmui_canScroll {
             setContentOffset(CGPoint(x: contentOffset.x, y: contentSize.height + contentInset.bottom - bounds.height), animated: animated)
         }
     }
 
     /// 等同于qmui_scrollToBottomAnimated(false)
-    func qmui_scrollToBottom() {
+    public func qmui_scrollToBottom() {
         qmui_scrollToBottomAnimated(false)
     }
 
     // 立即停止滚动，用于那种手指已经离开屏幕但列表还在滚动的情况。
-    func qmui_stopDeceleratingIfNeeded() {
+    public func qmui_stopDeceleratingIfNeeded() {
         if isDecelerating {
             setContentOffset(contentOffset, animated: false)
         }
