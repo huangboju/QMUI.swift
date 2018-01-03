7c31984f6c7f28e466e64b09eabfde96dc3976f1
Tony <tong.decula@163.com>
2018-01-03 14:17:00 +0800

    swiftformat 格式化
    Signed-off-by: Tony <tong.decula@163.com>

--- QMUI.swift/QMUIKit/UIKitExtensions/UITextView+QMUI.swift
+++ QMUI.swift/QMUIKit/UIKitExtensions/UITextView+QMUI.swift
@@ -12,50 +12,50 @@ extension UITextView {
      *  convert UITextRange to NSRangefor example, [self qmui_convertNSRangeFromUITextRange:self.markedTextRange]
      */
     public func qmui_convertNSRange(from textRange: UITextRange) -> NSRange {
-        let location = offset(from:beginningOfDocument, to:textRange.start)
-        let length = offset(from:textRange.start, to: textRange.end)
+        let location = offset(from: beginningOfDocument, to: textRange.start)
+        let length = offset(from: textRange.start, to: textRange.end)
         return NSMakeRange(location, length)
     }
-    
+
     /**
      *  设置 text 会让 selectedTextRange 跳到最后一个字符，导致在中间修改文字后光标会跳到末尾，所以设置前要保存一下，设置后恢复过来
      */
-    public func qmui_setTextKeepingSelectedRange(_ text: String) -> Void {
+    public func qmui_setTextKeepingSelectedRange(_ text: String) {
         let selectedTextRange = self.selectedTextRange
         self.text = text
         self.selectedTextRange = selectedTextRange
     }
-    
+
     /**
      *  设置 attributedText 会让 selectedTextRange 跳到最后一个字符，导致在中间修改文字后光标会跳到末尾，所以设置前要保存一下，设置后恢复过来
      */
-    public func qmui_setAttributedTextKeepingSelectedRange(_ attributedText: NSAttributedString) -> Void {
+    public func qmui_setAttributedTextKeepingSelectedRange(_ attributedText: NSAttributedString) {
         let selectedTextRange = self.selectedTextRange
         self.attributedText = attributedText
         self.selectedTextRange = selectedTextRange
     }
-    
+
     /**
      *  UITextView.scrollRangeToVisible() 并不会考虑 textContainerInset.bottom，所以使用这个方法来代替
      */
-    public func qmui_scrollCaretVisibleAnimated(_ animated: Bool) -> Void {
+    public func qmui_scrollCaretVisibleAnimated(_ animated: Bool) {
         if bounds.isEmpty {
             return
         }
-        
+
         let caret = caretRect(for: selectedTextRange!.end)
         // scrollEnabled 为 NO 时可能产生不合法的 rect 值 https://github.com/QMUI/QMUI_iOS/issues/205
         if caret.minX.isInfinite || caret.minY.isInfinite {
             return
         }
-        
+
         var contentOffsetY = contentOffset.y
-        
+
         if caret.minY == contentOffset.y + textContainerInset.top {
             // 命中这个条件说明已经不用调整了，直接 return，避免继续走下面的判断，会重复调整，导致光标跳动
             return
         }
-        
+
         if caret.minY < contentOffset.y + textContainerInset.top {
             // 光标在可视区域上方，往下滚动
             contentOffsetY = caret.minY - textContainerInset.top - contentInset.top
@@ -64,11 +64,9 @@ extension UITextView {
             contentOffsetY = caret.maxY - bounds.height + textContainerInset.bottom + contentInset.bottom
         } else {
             // 光标在可视区域内，不用调整
-            return;
+            return
         }
-        
+
         setContentOffset(CGPoint(x: contentOffset.x, y: contentOffsetY), animated: animated)
     }
-    
 }
-
