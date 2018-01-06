ab581098c19fca4d1ef3934e5c964a25073fbba7
李鑫 <605885892@qq.com>
2018-01-06 19:13:31 +0800

    [Lixin] finish UIImageView+QMUI.seift and update pregress.md

--- QMUI.swift/QMUIKit/UIKitExtensions/UIImageView+QMUI.swift
+++ QMUI.swift/QMUIKit/UIKitExtensions/UIImageView+QMUI.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImageView {
+    /**
+     *  把 UIImageView 的宽高调整为能保持 image 宽高比例不变的同时又不超过给定的 `limitSize` 大小的最大frame
+     *
+     *  建议在设置完x/y之后调用
+     */
+    public func qmui_sizeToFitKeepingImageAspectRatio(in limitSize: CGSize) {
+        guard let notNilImage = self.image else {
+            return
+        }
+
+        var currentSize = self.frame.size
+        if currentSize.width <= 0 {
+            currentSize.width = notNilImage.size.width
+        }
+        if currentSize.height <= 0 {
+            currentSize.height = notNilImage.size.height
+        }
+
+        let horizontalRatio = limitSize.width / currentSize.width
+        let verticalRatio = limitSize.height / currentSize.height
+        let ratio = min(horizontalRatio, verticalRatio)
+        var frame = self.frame
+        frame.size.width = flat(currentSize.width * ratio)
+        frame.size.height = flat(currentSize.height * ratio)
+        self.frame = frame
+    }
+}
