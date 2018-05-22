6f5623dbfbd0a77518cb978e51a2b52d85d559c3
Tony <tong.decula@163.com>
2018-05-22 09:49:09 +0800

    NSObject+QMUI.swift
    Signed-off-by: Tony <tong.decula@163.com>

--- QMUI.swift/QMUIKit/UIKitExtensions/NSObject+QMUI.swift
+++ QMUI.swift/QMUIKit/UIKitExtensions/NSObject+QMUI.swift
@@ -7,6 +7,7 @@
 //
 
 extension NSObject {
+
     /**
      判断当前类是否有重写某个父类的指定方法
 
@@ -15,19 +16,28 @@ extension NSObject {
      @return YES 表示子类有重写了父类方法，NO 表示没有重写（异常情况也返回 NO，例如当前类与指定的类并非父子关系、父类本身也无法响应指定的方法）
      */
     func qmui_hasOverrideMethod(selector: Selector, of superclass: AnyClass) -> Bool {
+        return NSObject.qmui_hasOverrideMethod(selector: selector, for: type(of: self), of: superclass)
+    }
 
-        if !type(of: self).isSubclass(of: superclass) {
-//            print("\(#function), \(NSStringFromClass(superclass)) 并非 \(NSStringFromClass(type(of: self))) 的父类")
+    /**
+     判断指定的类是否有重写某个父类的指定方法
+
+     @param selector 要判断的方法
+     @param superclass 要比较的父类，必须是当前类的某个 superclass
+     @return YES 表示子类有重写了父类方法，NO 表示没有重写（异常情况也返回 NO，例如当前类与指定的类并非父子关系、父类本身也无法响应指定的方法）
+     */
+    static func qmui_hasOverrideMethod(selector: Selector,
+                                       for aClass: AnyClass,
+                                       of superclass: AnyClass) -> Bool {
+        if !aClass.isSubclass(of: superclass) {
             return false
         }
-
         if !superclass.instancesRespond(to: selector) {
-//            print("\(#function), 父类 \(NSStringFromClass(superclass)) 自己本来就无法响应 \(NSStringFromSelector(selector)) 方法")
             return false
         }
 
         let superclassMethod = class_getInstanceMethod(superclass, selector)
-        let instanceMethod = class_getInstanceMethod(type(of: self), selector)
+        let instanceMethod = class_getInstanceMethod(aClass, selector)
         if instanceMethod == nil || instanceMethod == superclassMethod {
             return false
         }
@@ -35,70 +45,45 @@ extension NSObject {
     }
 
     /**
-     对 super 发送消息
-
-     @param aSelector 要发送的消息
-     @return 消息执行后的结果
-     @link http://stackoverflow.com/questions/14635024/using-objc-msgsendsuper-to-invoke-a-class-method
-     */
-    //    func qmui_performSelectorToSuperclass(aSelector: Selector) -> Any {
-    //        var mySuper = objc_super()
-    //        mySuper.receiver = self
-    //        mySuper.super_class = class_getSuperclass(object_getClass(self))
-    //        return
-    //    }
-
-    /**
-     对 super 发送消息
-
-     @param aSelector 要发送的消息
-     @param object 作为参数传过去
-     @return 消息执行后的结果
-     @link http://stackoverflow.com/questions/14635024/using-objc-msgsendsuper-to-invoke-a-class-method
-     */
-    //    func qmui_performSelectorToSuperclass(aSelector: Selector, with object: Any) -> Any {
-    //
-    //    }
-    
-    /**
      遍历当前实例的所有方法，父类的方法不包含在内
      */
-    func qmui_enumrateInstanceMethods(using handle: ((_ selector: Selector?) -> Void)?) {
-        NSObject.qmui_enumrateInstanceMethods(of: type(of: self),using: handle)
+    func qmui_enumrateInstanceMethodsUsingBlock(_ block: ((_ selector: Selector?) -> Void)?) {
+        NSObject.qmui_enumrateInstanceMethods(of: type(of: self), using: block)
     }
-    
+
     /**
      遍历指定的某个类的实例方法，该类的父类方法不包含在内
      *  @param aClass   要遍历的某个类
      *  @param block    遍历时使用的 block，参数为某一个方法
      */
-    static func qmui_enumrateInstanceMethods(of aClass: AnyClass, using handle: ((_ selector: Selector?) -> Void)?) {
-        var count:UInt32 = 0
-        let methods = class_copyMethodList(NSObject.self , &count)
-        for i in 0..<Int(count){
+    static func qmui_enumrateInstanceMethods(of _: AnyClass,
+                                             using block: ((_ selector: Selector?) -> Void)?) {
+        var count: UInt32 = 0
+        let methods = class_copyMethodList(NSObject.self, &count)
+        for i in 0 ..< Int(count) {
             if let method = methods?[i] {
                 let sel = method_getName(method)
-                handle?(sel)
+                block?(sel)
                 let name = String(cString: sel_getName(sel))
                 print(name)
             } else {
-                handle?(nil)
+                block?(nil)
             }
         }
     }
-    
-    
+
     /**
      遍历某个 protocol 里的所有方法
 
      @param protocol 要遍历的 protocol，例如 \@protocol(xxx)
      @param block 遍历过程中调用的 block
      */
-    static func qmui_enumerateProtocolMethods(ptc: Protocol, using handle: ((_ selector: Selector?) -> Void)?) {
+    static func qmui_enumerateProtocolMethods(ptc: Protocol,
+                                              using block: ((_ selector: Selector?) -> Void)?) {
         var methodCount: UInt32 = 0
         let methods = protocol_copyMethodDescriptionList(ptc, false, true, &methodCount)
         for i in 0 ..< methodCount {
-            handle?(methods?[Int(i)].name)
+            block?(methods?[Int(i)].name)
         }
     }
 }
